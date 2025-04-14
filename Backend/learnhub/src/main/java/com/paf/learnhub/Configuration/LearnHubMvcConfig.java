607afe31c1726a8d5b6885ff6ad09f8aa12a81d5
AshanDilakshana <167285793+AshanDilakshana@users.noreply.github.com>
2025-04-14 18:39:43 +0530

    fix

---
+++ Backend/learnhub/src/main/java/com/paf/learnhub/Configuration/LearnHubMvcConfig.java
@@ -0,0 +1,9 @@
+package com.paf.learnhub.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class LearnHubMvcConfig implements WebMvcConfigurer {
+    // No resource handlers needed
+}
