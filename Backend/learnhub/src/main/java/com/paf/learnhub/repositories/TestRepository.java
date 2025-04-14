7798820ba8b5564995582cdf05a85d2536b917b0
AshanDilakshana <167285793+AshanDilakshana@users.noreply.github.com>
2025-04-15 01:34:39 +0530

    Delete TestRepository.java

--- Backend/learnhub/src/main/java/com/paf/learnhub/repositories/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.paf.learnhub.repositories;
-
-
-import com.paf.learnhub.models.TestUser;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TestRepository extends MongoRepository<TestUser, String> {
-    // No custom queries for now
-}
