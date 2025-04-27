6cdb3f6f1b92dce7a971629eedd917169f82e2e6
charuka11513 <charukaanto11513@gmail.com>
2025-04-28 00:00:43 +0530

    deleted main files

--- Backend/learnhub/src/main/java/com/paf/learnhub/Configuration/LearnHubMvcConfig.java
+++
@@ -1,9 +0,0 @@
-package com.paf.learnhub.Configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class LearnHubMvcConfig implements WebMvcConfigurer {
-    // No resource handlers needed
-}
